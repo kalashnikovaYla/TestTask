ea74bc5d8b6cef0c2b3f81c295ae6d13ba69dec5
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-12 20:07:17 +0500

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by sss on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
